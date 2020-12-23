2304cf60361715f5d69ccb0434e73a3b6a49e974
Lukas Short <las6731@rit.edu>
2020-12-23 09:55:34 -0500

    import excalibur and fix indentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as ex from 'excalibur';
+import { Engine } from 'excalibur';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ts-minesweeper';
+export class AppComponent implements OnInit {
+    engine: Engine;
+
+    ngOnInit(): void {
+        this.engine = new ex.Engine({
+            canvasElementId: 'game',
+            width: 800,
+            height: 800
+        });
+
+        this.engine.start();
+    }
+
 }
